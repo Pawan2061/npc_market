0875a70e59bc4cb817448a6f191a811440110270
dipenbhat557 <pandeypawan2061@gmail.com>
2025-04-11 15:34:23 +0530

    feat: basic structure done

--- anchor/programs/npc_market/src/lib.rs
+++ anchor/programs/npc_market/src/lib.rs
@@ -8,63 +8,33 @@ declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 pub mod npc_market {
     use super::*;
 
-  pub fn close(_ctx: Context<CloseNpcMarket>) -> Result<()> {
-    Ok(())
-  }
+    pub fn initialize_npc_market() -> Result<()> {
+        Ok(());
+    }
 
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.npc_market.count = ctx.accounts.npc_market.count.checked_sub(1).unwrap();
-    Ok(())
-  }
+    pub fn mint_npc() -> Result<()> {
+        Ok(());
+    }
+    pub fn evolve_npc(...) -> Result<()> {
+        Ok(())
+    }
 
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.npc_market.count = ctx.accounts.npc_market.count.checked_add(1).unwrap();
-    Ok(())
-  }
+    pub fn burn_npc(...) -> Result<()> {
+        Ok(())
+    }
 
-  pub fn initialize(_ctx: Context<InitializeNpcMarket>) -> Result<()> {
-    Ok(())
-  }
+    pub fn update_npc_metadata(...) -> Result<()> {
+        Ok(())
+    }
 
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.npc_market.count = value.clone();
-    Ok(())
-  }
+    pub fn set_market_config(...) -> Result<()> {
+        Ok(())
+    }
 }
 
-#[derive(Accounts)]
-pub struct InitializeNpcMarket<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  init,
-  space = 8 + NpcMarket::INIT_SPACE,
-  payer = payer
-  )]
-  pub npc_market: Account<'info, NpcMarket>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseNpcMarket<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub npc_market: Account<'info, NpcMarket>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub npc_market: Account<'info, NpcMarket>,
-}
-
-#[account]
-#[derive(InitSpace)]
-pub struct NpcMarket {
-  count: u8,
-}
+// initialize_npc_market
+// mint_npc
+// evolve_npc
+// burn_npc
+// update_npc_metadata
+// set_market_config
